f92d2906724871622cb6d4055de4f7f03b239aa0
akshataw <wagheakshu@gmail.com>
2021-04-20 06:15:49 +0000

    setup terraform

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+    backend "remote" {
+        organization = "example-organization"
+        workspaces {
+            name = "example-workspace"
+        }
+    }
+}
+
+resource "aws_rds_cluster" "default" {
+    cluster_identifier = "pokemon-database"
+    engine = "aurora-mysql"
+    engine_mode = "serverless"
+    availability_zones = ["us-east-1a", "us-east-1b"]
+    database_name = var.name
+    master_username = var.user
+    master_password = var.password
+    backup_retention_period = 1
+    enable_http_endpoint = true
+    skip_final_snapshot = true
+}
