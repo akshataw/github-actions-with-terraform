2043a0602afd986eed70d607ce2e4ef16e01d3bb
akshataw <wagheakshu@gmail.com>
2021-04-22 09:55:37 +0000

    Update: remove ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,20 +42,6 @@ resource "aws_security_group" "ci-sg" {
         cidr_blocks = ["172.31.0.0/16"]
     }
 
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
     egress {
         from_port = 0
         to_port = 0
