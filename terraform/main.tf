cc5ae12aed842232b0c9b9fb1293b3bbecdbfdc1
akshataw <wagheakshu@gmail.com>
2021-04-22 10:07:54 +0000

    Update: ingress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "ci-sg" {
         cidr_blocks = ["172.31.0.0/16"]
     }
 
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["172.31.0.0/16"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
