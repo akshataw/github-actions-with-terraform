a3241b753d151bc345bbf2d44ad39a91421d74c8
akshataw <wagheakshu@gmail.com>
2021-04-22 07:44:04 +0000

    Update: db instace

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,21 +12,16 @@ terraform {
     }
 }
 
-resource "aws_rds_cluster" "default" {
-    cluster_identifier = "pokemon-database"
-    engine = "aurora-mysql"
-    engine_mode = "serverless"
-    availability_zones = ["us-east-1a", "us-east-1b"]
-    database_name = var.name
-    master_username = var.user
-    master_password = var.password
-    backup_retention_period = 1
-    enable_http_endpoint = true
-    skip_final_snapshot = true
-
-    lifecycle {
-      prevent_destroy = true
-    }
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mycicddb"
+  username             = var.user
+  password             = var.password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
 
 resource "aws_security_group" "ci-sg" {
@@ -58,32 +53,6 @@ resource "aws_security_group" "ci-sg" {
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    lifecycle {
-      prevent_destroy = true
-    }
-}
-
-resource "aws_security_group" "cd-sg" {
-    name = "cd-sg"
-    description = "Allow TLS inbound traffic for CD Demo"
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["172.31.0.0/16"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    lifecycle {
-      prevent_destroy = true
     }
 }
 
