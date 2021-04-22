09f107483f64f8e205086a802b7636ad4e3c6e25
akshataw <wagheakshu@gmail.com>
2021-04-22 05:39:49 +0000

    Update: security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,13 +25,35 @@ resource "aws_rds_cluster" "default" {
     skip_final_snapshot = true
 }
 
-resource "aws_vpc" "cimain" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_security_group" "ci-sg" {
+    name = "ci-sg"
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["172.31.0.0/16"]
+    }
 
-  tags = {
-    Name = "cimain"
-  }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 variable "name" {
