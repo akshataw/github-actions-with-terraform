c75dbb84055c1fb843922a061ba0cb3c2cc814b4
akshataw <wagheakshu@gmail.com>
2021-04-22 09:31:59 +0000

    Update: remove rds

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,18 +12,6 @@ terraform {
     }
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mycicddb"
-  username             = var.user
-  password             = var.password
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
-
 resource "aws_security_group" "ci-sg" {
     name = "ci-sg"
     description = "Allow TLS inbound traffic for CI/CD Demo"
